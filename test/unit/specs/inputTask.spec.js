9cf540b2e2bce7f3ba0debb64f0b0c5848e5f523
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-07-03 01:24:00 -0300

    built tests for inputTask component

--- test/unit/specs/inputTask.spec.js
+++ test/unit/specs/inputTask.spec.js
@@ -1,10 +1,34 @@
 import Vue from 'vue';
 import InputTask from 'src/components/InputTask';
 
+
+
 describe('InputTask.vue', () => {
- it('Should render correct contents', () => {
+ let vm = {};
+ beforeEach(function() {
   const Constructor = Vue.extend(InputTask);
   const vm = new Constructor().$mount();
+ })
+ it('Should render correct contents', () => {
+
   expect(vm.$el.querySelectorAll('.new-todo').length).to.equal(1);
+ })
+ it('should create the task correctly', () => {
+ 
+  let task = vm.createTask('to buy milk');
+  expect(task.title).to.equal('to buy milk');
+  expect(task.completed).to.be.false;
+ })
+ it('should clean the input', () => {
+
+  vm.$el.querySelector('.new-todo').value = 'to buy milk';
+  vm.clearField();
+  expect(vm.$el.querySelector('.new-todo').value).to.equal('');
+ })
+ it('should call the event', () => {
+  let spy = sinon.spy();
+  vm.$on('newTask', spy);
+  vm.broadcast();
+  expect(spy).to.hava.been.called;
  })
 })
