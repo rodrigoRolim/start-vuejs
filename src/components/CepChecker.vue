59ab9fbe12f71734c8bd27ddec323ca417db7ee0
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-07-07 12:36:31 -0300

    created ceps component

---
+++ src/components/CepChecker.vue
@@ -0,0 +1,15 @@
+<template>
+ <section class="cepChecker">
+  <label for="">Digite seu cep</label>
+  <input type="text"/>
+ </section>
+</template>
+<script>
+ export default {
+  data (){
+   return {
+    
+   }
+  }
+ }
+</script>
